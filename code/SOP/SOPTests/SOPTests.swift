ad51f91b9791a8bb798335c26b78393eca1efcd5
Bas Broek <bas@basbroek.nl>
2016-03-17 08:47:28 +0100

    Added code

---
+++ code/SOP/SOPTests/SOPTests.swift
@@ -0,0 +1,26 @@
+//
+//  SOPTests.swift
+//  SOPTests
+//
+//  Created by Bas Broek on 17/03/16.
+//  Copyright © 2016 Bas Broek. All rights reserved.
+//
+
+import XCTest
+@testable import SOP
+
+class SOPTests: XCTestCase {
+  
+  override func setUp() {
+    super.setUp()
+  }
+  
+  override func tearDown() {
+    super.tearDown()
+  }
+  
+  func testExample() {
+    XCTAssertTrue(1 == 1)
+    XCTAssertFalse(1 == 2)
+  }
+}
