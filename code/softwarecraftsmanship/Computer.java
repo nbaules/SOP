d2bc88f797820ac0bb483416be3695d540b17d16
Bas Broek <bas@basbroek.nl>
2016-03-31 10:10:17 +0200

    Removed numberOfParts(), removed else clauses in isComplet()
    Removed numberOfParts() in favor of direct accessor (parts.size())

--- code/softwarecraftsmanship/Computer.java
+++ code/softwarecraftsmanship/Computer.java
@@ -6,24 +6,15 @@ public class Computer {
     
     List<Part> parts = new ArrayList<>();
     
-    public int numberOfParts() {
-        int count = 0;
-        for (Part part: parts) { 
-            count++;
-        }
-        return count;
-    }
-    
     public void addPart(Part p) {
         parts.add(p);
     }
     
     public double getPrice() {
         double price = 0.0;
-        
-        for (int i = 0; i < numberOfParts(); i++) {
-            Part part = parts.get(i);
-            price += part.getPrice();
+
+        for (Part part: parts) {
+            price = price + part.getPrice();
         }
         
         return price;
@@ -38,28 +29,22 @@ public class Computer {
         for (Part part: parts) {
             if (part.getType().equals("Casing")) {
                 hasCasing = true;
+                continue;
             }
-            else {
-                if (part.getType().equals("Processor")) {
-                    hasProcessor = true;
-                }
-                else {
-                    if (part.getType().equals("Memory")) {
-                        hasMemory = true;
-                    }
-                    else {
-                        if (part.getType().equals("Motherboard")) {
-                            hasMotherboard = true;
-                        }
-                    }
-                }
+            if (part.getType().equals("Processor")) {
+                hasProcessor = true;
+                continue;
+            }
+            if (part.getType().equals("Memory")) {
+                hasMemory = true;
+                continue;
+            }
+            if (part.getType().equals("Motherboard")) {
+                hasMotherboard = true;
+                continue;
             }
         }
         
-        if (!hasCasing || !hasProcessor || !hasMemory || !hasMotherboard) {
-            return false;
-        } else {
-            return true;
-        }
+        return hasCasing && hasProcessor && hasMemory && hasMotherboard;
     }
 }
