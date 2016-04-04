b338017a965781ec0bdd686135def98559b9a42c
Bas Broek <bas@basbroek.nl>
2016-04-04 20:16:14 +0200

    Removed else clause

--- code/softwarecraftsmanship/Main.java
+++ code/softwarecraftsmanship/Main.java
@@ -27,11 +27,6 @@ public class Main {
                 StringWrapperFactory.createStringWrapperWithString("MSI H97 PC MATE - Socket 1150 - ATX"), 
                 DoubleWrapperFactory.createDoubleWrapperWithDouble(89.0)));
         
-        if (computer.isComplete().booleanValue()) {
-            System.out.println("Total price is EUR " + computer.getPrice());
-        }
-        else {
-            System.out.println("Your computer is not ready yet...");
-        }
+        System.out.println((computer.isComplete().booleanValue()) ? "Total price is EUR " + computer.getPrice() : "Your computer is not ready yet...");
     }
 }
